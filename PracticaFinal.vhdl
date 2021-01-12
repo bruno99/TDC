456e88b9ab5040e36bcb03c4dedacbf9676601dd
Bruno Urbán <43136694+bruno99@users.noreply.github.com>
2021-01-12 18:29:14 +0100

    Update PracticaFinal.vhdl

--- PracticaFinal.vhdl
+++ PracticaFinal.vhdl
@@ -18,10 +18,9 @@ architecture Behavioral of Maquina_Expendedora is
 
 begin
 
- process (CLK,RESET) begin
+ process (CLK) begin
     if(RESET='1') then
       estado <= s0;
-      COIN_IN <= "000" ; --se devolverá todo lo metido
     elsif rising_edge(CLK) then
       estado <= estado_siguiente; 
     end if;
@@ -30,28 +29,27 @@ begin
  process(COIN_IN,COIN_OUT,LATA, estado, estado_siguiente) begin
       case (estado) is
           when s0 =>
-             if(COIN_IN <= "001") then estado_siguiente <= s1; --1€ metido va al s1 a la espera de más dinero
+             if(COIN_IN = "001") then estado_siguiente <= s1; --1€ metido va al s1 a la espera de más dinero
                else if (COIN_IN != "001") then estado_siguiente <= s2;--Cualquier cantidad distinta a 1€ lleva al estado 2, donde dependiendo de la cantidad se verá                
               end if;
           when s1 =>   
-             if(COIN_IN <= "001") then COIN_IN <= "010" & estado_siguiente <= s2;--si se mete otro 1€ el total son 2€
-               else if(COIN_IN <= "010") then COIN_IN <= "011" & estado_siguiente <= s2; --si se mete otro 2€ el total son 3€
-                 else if(COIN_IN <= "101") then COIN_IN <= "110" & estado_siguiente <= s2;--si se mete otro 5€ el total son 6€ 
-                   else if (RESET <= '1') then COIN_OUT <= "001" & estado_siguiente <= s0;;--si se anula se devuelve 1€
+             if(COIN_IN = "001") then estado_siguiente <= s2;--si se mete otro 1€ el total son 2€
+               else if(COIN_IN = "010") then COIN_IN <= "011" & estado_siguiente <= s2; --si se mete otro 2€ el total son 3€
+                 else if(COIN_IN = "101") then COIN_IN <= "110" & estado_siguiente <= s2;--si se mete otro 5€ el total son 6€ 
+                   else if (RESET = '1') then COIN_OUT <= "001" & estado_siguiente <= s0;;--si se anula se devuelve 1€
                    end if;
                  end if;
                 end if;
           when s2 =>
-             if(COIN_IN <= "010") then COIN_OUT <= "000";  --Si lo introducido es justo se devuelven 0€
-               else if(COIN_IN <= "011")then COIN_OUT <= "001";  --Si lo introducido es 3€ se devuelven 1€
-                 else if(COIN_IN <= "101")then COIN_OUT <= "011";--Si lo introducido es 5€ se devuelven 3€
-                  else if(COIN_IN <= "110")then COIN_OUT <= "100";--Si lo introducido es 6€ se devuelven 4€
+             if(COIN_IN = "010") then COIN_OUT <= "000";  --Si lo introducido es justo se devuelven 0€
+               else if(COIN_IN = "011")then COIN_OUT <= "001";  --Si lo introducido es 3€ se devuelven 1€
+                 else if(COIN_IN = "101")then COIN_OUT <= "011";--Si lo introducido es 5€ se devuelven 3€
+                  else if(COIN_IN = "110")then COIN_OUT <= "100";--Si lo introducido es 6€ se devuelven 4€
                   end if;
                  end if;   
                end if;
              end if;    
              LATA <= '1';--Se activa y recibe la lata
-             RESET <= '1';--para que la maquina vuelva al estado inicial
           when others => null;--para casos excepcionales
       end case;
  end process;
