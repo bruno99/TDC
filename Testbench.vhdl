767d8dc17791333ff45dcb49c88a21d763096966
Bruno Urbán <43136694+bruno99@users.noreply.github.com>
2021-01-13 17:20:50 +0100

    Update Testbench.vhdl

--- Testbench.vhdl
+++ Testbench.vhdl
@@ -13,7 +13,7 @@ end tb_Maquina_Expendedora;
     port(
 CLK : in std_logic; --Clock
 RESET: in std_logic;
-COIN_IN in std_logic_vector( 2 downto 0); 
+COIN_IN: in std_logic_vector( 2 downto 0); 
 COIN_OUT: out std_logic_vector( 2 downto 0); 
 LATA: out std_logic
 );
@@ -21,38 +21,36 @@ LATA: out std_logic
    
     -- Definir señales
     signal CLK, RESET: std_logic;
-    signal COIN_IN in std_logic_vector( 2 downto 0); 
-    signal COIN_OUT: out std_logic_vector( 2 downto 0); 
-    signal LATA: out std_logic;
+    signal COIN_IN:  std_logic_vector( 2 downto 0); 
+    signal COIN_OUT:  std_logic_vector( 2 downto 0); 
+    signal LATA:  std_logic;
     
+  begin  
     -- Instanciamos tb_Maquina_Expendedora
-    DUT : tb_Maquina_Expendedora port map(
+    DUT:  Maquina_Expendedora port map(
         CLK   => CLK,
-        RESET  => RESET
+        RESET  => RESET,
         COIN_IN  => COIN_IN,
         COIN_OUT => COIN_OUT,
         LATA =>LATA
-       
     );
-   
-begin
-     process(CLK)
-   begin
+   process begin
+    CLK <= '0'; wait for 10ns;
+    CLK <= '1'; wait for 15ns;
+end process;
+process begin 
+    RESET <= '0'; wait;
+end process;
+     process  begin
+  
      --PROBAMOS TODAS LAS POSIBILIDADES
-    
-    COIN_IN <= "001"; RESET <= '1';  wait 10 ns;    --LATA será 0 y COIN_OUT será 1 
-    COIN_IN <= "010"; RESET <= '1';  wait 10 ns;    --LATA será 0 y COIN_OUT será 2
-    COIN_IN <= "011"; RESET <= '1';  wait 10 ns;    --LATA será 0 y COIN_OUT será 3
-    COIN_IN <= "100"; RESET <= '1';  wait 10 ns;    --LATA será 0 y COIN_OUT será 4
-    COIN_IN <= "101"; RESET <= '1';  wait 10 ns;    --LATA será 0 y COIN_OUT será 5
-    COIN_IN <= "110"; RESET <= '1';  wait 10 ns;    --LATA será 0 y COIN_OUT será 6
-    
-    COIN_IN <= "001"; RESET <= '0';  wait 10 ns;    --LATA será 0 y COIN_OUT será 0 a la espera de más órdenes
-    COIN_IN <= "010"; RESET <= '0';  wait 10 ns;    --LATA será 1 y COIN_OUT será 0
-    COIN_IN <= "011"; RESET <= '0';  wait 10 ns;    --LATA será 1 y COIN_OUT será 1
-    COIN_IN <= "100"; RESET <= '0';  wait 10 ns;    --LATA será 1 y COIN_OUT será 2
-    COIN_IN <= "101"; RESET <= '0';  wait 10 ns;    --LATA será 1 y COIN_OUT será 3
-    COIN_IN <= "110"; RESET <= '0';  wait 10 ns;    --LATA será 1 y COIN_OUT será 4
+     
+    COIN_IN <= "001";   wait for 10 ns;    --LATA será 0 y COIN_OUT será 0 a la espera de más órdenes
+    COIN_IN <= "010";   wait for 10 ns;    --LATA será 1 y COIN_OUT será 0
+    COIN_IN <= "011";   wait for 10 ns;    --LATA será 1 y COIN_OUT será 1
+    COIN_IN <= "100";   wait for 10 ns;    --LATA será 1 y COIN_OUT será 2
+    COIN_IN <= "101";   wait for 10 ns;    --LATA será 1 y COIN_OUT será 3
+    COIN_IN <= "110";   wait for 10 ns;    --LATA será 1 y COIN_OUT será 4
 
    
         end process;
